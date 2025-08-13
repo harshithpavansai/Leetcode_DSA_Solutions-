5bb02902ce77451100f467efff4e12e3f9384f15
harshithpavansai <harshithpavansai188@gmail.com>
2025-08-13 08:50:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.5 MB (100.00%)

---
+++ 2571-find-the-pivot-integer/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum=(n*(n+1))/2;
+        int sq = (int)Math.sqrt(sum);
+        if(sq*sq==sum){
+            return sq;
+        }
+        else{
+            return -1;
+        }
+    }
+}
