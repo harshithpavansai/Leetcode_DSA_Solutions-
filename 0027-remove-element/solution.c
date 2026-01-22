46f6a69e406a440986c506733430b942e7828ad6
harshithpavansai <harshithpavansai188@gmail.com>
2026-01-22 06:15:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (100.00%)

--- 0027-remove-element/solution.c
+++ 0027-remove-element/solution.c
@@ -3,7 +3,8 @@ int removeElement(int* nums, int numsSize, int val) {
     int k;
     for(int i=0;i<numsSize;i++){
         if (nums[i]!=val){
-        nums[k++]=nums[i];
+        nums[k]=nums[i];
+        k++;
         }
     }
     return k;
