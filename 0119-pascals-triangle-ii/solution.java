6a417382e55e8da0b3c1a2f2401575058b399edc
harshithpavansai <harshithpavansai188@gmail.com>
2026-03-05 06:41:24 +0000

    Sync LeetCode submission Runtime - 2 ms (25.81%), Memory - 42.7 MB (19.01%)

---
+++ 0119-pascals-triangle-ii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        int numRows=rowIndex + 1;
+         List<List<Integer>> res = new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> val = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0 || j== i){
+                    val.add(1);
+                }
+                else{
+                    List<Integer> prev = res.get(i - 1);
+                    val.add(prev.get(j - 1) + prev.get(j));
+                   
+                }
+            }
+             res.add(val);
+        }
+    return res.get(rowIndex);
+    }
+}
+
+
+
+
+
+
