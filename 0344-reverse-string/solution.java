115a754181276a4b0c20bf70091f38d6633225fe
harshithpavansai <harshithpavansai188@gmail.com>
2026-07-20 09:06:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.1 MB (88.33%)

--- 0344-reverse-string/solution.java
+++ 0344-reverse-string/solution.java
@@ -1,15 +1,18 @@
 class Solution {
     public void reverseString(char[] s) {
-        char arr[] = new char[s.length]; // Temporary array
-
-        // First loop: copy characters from s[] to arr[] in reverse order
-        for (int i = s.length - 1, j = 0; i >= 0; i--, j++) {
-            arr[j] = s[i];
+        // char arr[] = new char[s.length];
+        // for (int i = s.length - 1, j = 0; i >= 0; i--, j++) {
+        //     arr[j] = s[i];
+        // }
+        // for (int i = 0; i < s.length; i++) {
+        //     s[i] = arr[i];
+        // }
+        int n = s.length;
+        for(int i=0;i<n/2;i++){
+            char temp=s[i];
+            s[i]=s[n-i-1];
+            s[n-i-1]=temp;
         }
-
-        // Second loop: copy reversed characters back to s[]
-        for (int i = 0; i < s.length; i++) {
-            s[i] = arr[i];
-        }
+        
     }
 }
