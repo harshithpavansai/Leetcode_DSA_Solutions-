76bb6c26a13120f0fb02fe548dfff5619d63ec05
harshithpavansai <harshithpavansai188@gmail.com>
2026-07-31 14:43:39 +0000

    Sync LeetCode submission Runtime - 3 ms (16.70%), Memory - 47.4 MB (91.67%)

---
+++ 0073-set-matrix-zeroes/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        HashSet<Integer> rows = new HashSet<>();
+        HashSet<Integer> cols = new HashSet<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rows.contains(i) || cols.contains(j)){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
