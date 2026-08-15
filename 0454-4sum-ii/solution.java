1ce02f273e67db6ce91dc2b1670c0fbf62468fa8
harshithpavansai <harshithpavansai188@gmail.com>
2026-08-15 18:35:28 +0000

    Sync LeetCode submission Runtime - 109 ms (80.99%), Memory - 47 MB (53.88%)

---
+++ 0454-4sum-ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int cnt=0;
+        for(int a : nums1){
+            for(int b : nums2){
+                int sum = a+b;
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        }
+        for(int c : nums3){
+                for(int d : nums4){
+                    int sum = c+d;
+                    cnt+=map.getOrDefault(-sum,0);
+                }
+            }
+        return cnt;
+    }
+}
