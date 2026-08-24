a1c58988e913ea5a1dbf52ba6412adc12d950c4b
harshithpavansai <harshithpavansai188@gmail.com>
2026-08-24 09:49:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.1 MB (51.80%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,27 +1,54 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[2];
-        arr[0] = -1;
-        arr[1] = -1;
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(target == nums[i]) {
-                arr[0] = i;
-                break;
-            }
+
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
         }
         
-        if(arr[0] == -1) return arr;
-        
-        for(int i = arr[0]; i < nums.length; i++) {
-            if(target == nums[i]) {
-                arr[1] = i;
-            } else {
-                break;
+        int fi=-1;
+        int li=-1;
+        int l = 0;
+        int r = nums.length-1;
+        int mid=0;
+        while(l<r){
+            mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                fi=mid;
+                r = mid;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid;
             }
         }
+        if(nums[l]==target){
+            fi=l;
+        }
+        li=fi;
+        int i=fi;
+        int j=nums.length-1;
+        if (fi == -1) {
+            return new int[]{-1, -1};
+        }
+        while(i<j){
+            int miid = i+(j-i)/2;
+            if(nums[miid]==target){
+                li=miid;
+                i=miid+1;
+            }
+            else if(nums[miid]>target){
+                j=miid;
+            }
+            else{
+                i=miid+1;
+            }
+        }
+        if(nums[i]==target)
+        li=i;
         
-        return arr;
+        return new int[]{fi,li};
     }
 }
 
